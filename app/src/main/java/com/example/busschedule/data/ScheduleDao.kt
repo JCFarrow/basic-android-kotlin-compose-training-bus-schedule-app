c98adf51f3d67833d29fea4443707bfb84f3ca03
Joel <9orangetigers@gmail.com>
2024-02-02 23:16:17 +1300

    Fix crash on startup

--- app/src/main/java/com/example/busschedule/data/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/data/ScheduleDao.kt
@@ -6,9 +6,9 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ScheduleDao {
-    @Query("SELECT * FROM schedule ORDER BY arrivalTimeInMillis")
+    @Query("SELECT * FROM schedule ORDER BY arrival_time")
     fun getSchedule(): Flow<List<BusSchedule>>
 
-    @Query("SELECT * FROM schedule WHERE stopName = :stopName ORDER BY arrivalTimeInMillis")
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time")
     fun getStopSchedule(stopName: String): Flow<List<BusSchedule>>
 }
